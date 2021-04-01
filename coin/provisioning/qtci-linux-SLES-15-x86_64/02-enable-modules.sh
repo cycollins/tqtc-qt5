02208204258ea3d737edb3c304ab57b20b37fa9d
Heikki Halmet <heikki.halmet@qt.io>
2021-04-01 09:20:00 +0300

    Provisioning: Enable needed repositories for SLES 15
    Change-Id: I70a68d3e61f221cb046ff3c5ead823a0df04357b
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@qt.io>

--- coin/provisioning/qtci-linux-SLES-15-x86_64/02-enable-modules.sh
+++ coin/provisioning/qtci-linux-SLES-15-x86_64/02-enable-modules.sh
@@ -3,6 +3,7 @@
 set -ex
 
 # Activate these modules
-
+sudo SUSEConnect -p sle-module-desktop-applications/15/x86_64
+sudo SUSEConnect -p sle-module-development-tools/15/x86_64
 # This is needed by Nodejs and QtWebEngine
 sudo SUSEConnect -p sle-module-web-scripting/15/x86_64
